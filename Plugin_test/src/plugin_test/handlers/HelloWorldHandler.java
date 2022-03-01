816bf8f46720da5c98a757023ea5d528494894cd
Kimjongsoo <31787166+Kimjongsoo@users.noreply.github.com>
2022-03-01 17:20:50 +0900

    start dialog

--- Plugin_test/src/plugin_test/handlers/HelloWorldHandler.java
+++ Plugin_test/src/plugin_test/handlers/HelloWorldHandler.java
@@ -1,10 +1,13 @@
 package plugin_test.handlers;
 
+import java.awt.Dialog;
+
 import javax.inject.Named;
+import javax.swing.JButton;
+import javax.swing.JDialog;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.services.IServiceConstants;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 
 /** <b>Warning</b> : 
@@ -16,7 +19,13 @@ public class HelloWorldHandler {
 	@Execute
 	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell s) {
 		
-		MessageDialog.openInformation(s, "E4 Information Dialog", "Hello world from a pure Eclipse 4 plug-in");
+		//MessageDialog.openInformation(s, "E4 Information Dialog", "Hello world from a pure Eclipse 4 plug-in");
+		Dialog dialog = new JDialog();
+		dialog.setTitle("나의 다이얼로그");
+		dialog.add(new JButton("click!"));
+		dialog.setSize(300,300);
+		dialog.setVisible(true);
+		
 
 	}
 
