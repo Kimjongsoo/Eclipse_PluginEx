a4f66a79f46d351476ebac75e9441a15b7c24999
Kimjongsoo <31787166+Kimjongsoo@users.noreply.github.com>
2022-03-04 16:15:54 +0900

    update

--- Plugin_test/src/plugin_test/handlers/SampleHandler.java
+++ Plugin_test/src/plugin_test/handlers/SampleHandler.java
@@ -37,51 +37,15 @@ public class SampleHandler extends AbstractHandler {
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"Plugin_test",
-				"Hello, Eclipse world");
+//		MessageDialog.openInformation(
+//				window.getShell(),
+//				"Plugin_test",
+//				"Hello, Eclipse world");
 		
 		UI dialog = new UI(window.getShell(),SWT.DIALOG_TRIM);
 		dialog.open();
-		
-//			String text = "Window Text";
-//			String btntext = "Button Text";
-//			String welcomtext = "Hello World";
-//
-//			Display display = new Display();
-//	        Shell shell = window.getShell();
-//	        shell.setText(text);
-//	        //shell.setLayout(new Layout());
-//	        shell.setLayout(new FillLayout());
-//
-//	        // 버튼 생성
-//	        Button button1 = new Button(shell, 0);
-//	        button1.setBounds(10, 10, 150, 30);
-//	        button1.setText(btntext);
-//
-//	        // 텍스트박스 생성
-//	        Text textBox1 = new Text(shell, 2626);
-//	        String font = "굴림";
-//	        textBox1.setLayoutData(new GridData(1808));
-//	        textBox1.setFont(new Font(display, font, 20, 0));
-//	        textBox1.setText(welcomtext);
-//	        textBox1.setFocus();
-//
-//	        // 레이블 생성
-//	        Label label1 = new Label(shell, 0);
-//	        label1.setBounds(200, 10, 150, 30);
-//	        label1.setText(welcomtext);
-//
-//	        shell.open();
-//	        while (!shell.isDisposed()) {
-//	            if (!display.readAndDispatch()) {
-//	                display.sleep();
-//	            }
-//	        }
-//	        display.dispose();
+
 	    
-		
 		
 		return null;
 	}
--- Plugin_test/src/plugin_test/handlers/UI.java
+++ Plugin_test/src/plugin_test/handlers/UI.java
@@ -1,10 +1,15 @@
 package plugin_test.handlers;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 public class UI extends Dialog{
 	
@@ -33,15 +38,46 @@ public class UI extends Dialog{
 	}
 
 	private void createContents() {
-		// TODO Auto-generated method stub
+		
+		Text title;
+		Button btnGo;
+		Button btnReset;
+		Button btnSearch;
+		Button btnDBquery;
+
 		shell = new Shell(getParent(), SWT.DIALOG_TRIM|SWT.RESIZE);
-		shell.setSize(660,390);
+		shell.setSize(750,600);
 		org.eclipse.swt.graphics.Rectangle screenSize = shell.getDisplay().getPrimaryMonitor().getBounds();
 		shell.setLocation((screenSize.width - shell.getBounds().width) /2,
 				(screenSize.height -shell.getBounds().height) /2);
 		shell.setText(getText());
-		Button btnNewButton = new Button(shell, SWT.NONE);
 		
+		title = new Text(shell, SWT.BORDER);
+		title.setBounds(50, 19, 400, 25);
+		title.setText("단순 조회 자바 소스 GENERATOR");
+		title.setBackground(title.getDisplay().getSystemColor(SWT.COLOR_YELLOW));
+		Font font = new Font(title.getDisplay(), new FontData("굴림체", 14, SWT.NORMAL));
+		title.setFont(font);
+		title.setEditable(false);
+		
+		btnGo = new Button(shell, SWT.NONE);
+		btnGo.addSelectionListener(new SelectionAdapter(){
+			public void widgetSelected(SelectionEvent e) {
+				
+			}
+		});
+		
+		btnGo.setBounds(530, 19, 76, 25);
+		btnGo.setText("실행");
+		
+		btnReset = new Button(shell, SWT.NONE);
+		btnReset.addSelectionListener(new SelectionAdapter(){
+			public void widgetSelected(SelectionEvent e) {
+				
+			}
+		});
+		btnReset.setBounds(620, 19, 85, 25);
+		btnReset.setText("초기화");
 	}
 
 }
